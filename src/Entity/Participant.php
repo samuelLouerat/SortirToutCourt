8ff17d2b610de860445755ca30be9fa195b4fdc5
fherault2021 <herault.fanny@gmail.com>
2022-04-11 14:51:30 +0200

    Création + config de User

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,6 +6,7 @@ use App\Repository\ParticipantRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ParticipantRepository::class)]
 class Participant implements UserInterface, PasswordAuthenticatedUserInterface
