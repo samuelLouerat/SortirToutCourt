4f91cb1efe2a70479fccdbb53c833b831bc52ef6
mchevalier2021 <mathilde.chevalier2021@campus-eni.fr>
2022-04-12 09:13:23 +0200

    2001 - Début liste des évènements - MC

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $nom;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateHeureDebut;
+
+    #[ORM\Column(type: 'dateinterval')]
+    private $duree;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateLimiteInscription;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $nbInscriptionMax;
+
+    #[ORM\Column(type: 'string', length: 1500)]
+    private $infosSortie;
+
+    #[ORM\ManyToOne(targetEntity: Participant::class, inversedBy: 'SortieOrganise')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $organisateur;
+
+    #[ORM\ManyToMany(targetEntity: Participant::class, mappedBy: 'estInscrit')]
+    private $participants;
+
+    #[ORM\ManyToOne(targetEntity: Campus::class, inversedBy: 'sorties')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $siteOrganisateur;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(\DateTimeInterface $dateHeureDebut): self
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateInterval
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(\DateInterval $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateLimiteInscription(): ?\DateTimeInterface
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    public function setDateLimiteInscription(\DateTimeInterface $dateLimiteInscription): self
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+
+        return $this;
+    }
+
+    public function getNbInscriptionMax(): ?int
+    {
+        return $this->nbInscriptionMax;
+    }
+
+    public function setNbInscriptionMax(?int $nbInscriptionMax): self
+    {
+        $this->nbInscriptionMax = $nbInscriptionMax;
+
+        return $this;
+    }
+
+    public function getInfosSortie(): ?string
+    {
+        return $this->infosSortie;
+    }
+
+    public function setInfosSortie(string $infosSortie): self
+    {
+        $this->infosSortie = $infosSortie;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?Participant
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(?Participant $organisateur): self
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Participant>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(Participant $participant): self
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants[] = $participant;
+            $participant->addEstInscrit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(Participant $participant): self
+    {
+        if ($this->participants->removeElement($participant)) {
+            $participant->removeEstInscrit($this);
+        }
+
+        return $this;
+    }
+
+    public function getSiteOrganisateur(): ?Campus
+    {
+        return $this->siteOrganisateur;
+    }
+
+    public function setSiteOrganisateur(?Campus $siteOrganisateur): self
+    {
+        $this->siteOrganisateur = $siteOrganisateur;
+
+        return $this;
+    }
+
+}
