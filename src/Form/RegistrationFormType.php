d15cb4f6a1b7fb5c8ad7cb4de3be2f593bfb2e92
mchevalier2021 <mathilde.chevalier2021@campus-eni.fr>
2022-04-12 15:21:09 +0200

    2001 - Event List - MC

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -22,6 +24,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('phone')
             ->add('pseudo')
+            ->add('campus',EntityType::class, ['class'=>Campus::class, 'choice_label'=>'name'])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
