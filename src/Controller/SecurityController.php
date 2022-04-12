27f318ff8758afa92bd5fa96124ae90e8d393304
slouerat2021 <samuel.louerat2021@campus-eni.fr>
2022-04-12 12:28:38 +0200

    S'enregistrer, se connecter, se souvenir de moi

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('sortie_liste');
+             return $this->redirectToRoute('event_list');
          }
 
         // get the login error if there is one
