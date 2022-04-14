4a14188dc85dda827b077c498dd89f3a9abf9947
fherault2021 <herault.fanny@gmail.com>
2022-04-14 09:46:37 +0200

    myprofile fonctionnel

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,34 +2,68 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-
+            ->add('imageFile',
+                VichImageType::class, [
+                    'required' => false,
+                    'download_uri' => false,
+                    'allow_delete' => false,
+                    'image_uri' => true
+                ])
             ->add('pseudo', TextType::class,
-            ["label"=>'Pseudo'])
-            ->add('firstName',TextType::class,
-                ["label"=>'Prénom'] )
+                ["label" => 'Pseudo'])
+            ->add('firstName', TextType::class,
+                ["label" => 'Prénom'])
             ->add('lastName', TextType::class,
-                ["label"=>'Nom'])
+                ["label" => 'Nom'])
             ->add('phone', TextType::class,
-                ["label"=>'Téléphone'])
+                ["label" => 'Téléphone'])
             ->add('email', TextType::class,
-                ["label"=>'Email'] )
-            ->add('password', TextType::class,
-                ["label"=>'Mot de passe'])
-
-        ;
+                ["label" => 'Email'])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Saisir à nouveau le mot de passe'],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ]
+            ])
+            ->add('campus', EntityType::class,
+                ['class' => Campus::class, 'choice_label' => 'name'])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
